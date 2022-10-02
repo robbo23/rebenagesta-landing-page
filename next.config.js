1ff04ee24be0cff59c01f3ee9ee3bfe5a040ef96
Robbo23 <47586046+Robbo23@users.noreply.github.com>
2022-10-02 05:31:10 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -12,6 +12,23 @@ const nextConfig = {
   },
   experimental: {
     nextScriptWorkers: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ]
+      }
+    ];
   }
 };
 
